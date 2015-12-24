6d8a7297c9f6159ed49c54cc914b54863a0b42e7
smithsf0x <smithsf0x@gmail.com>
2015-12-24 22:32:27 +0100

    Working

--- js/index.js
+++ js/index.js
@@ -2,19 +2,29 @@
 * @Author: smithsf0x
 * @Date:   2015-12-19 13:30:47
 * @Last Modified by:   smithsf0x
-* @Last Modified time: 2015-12-19 13:30:58
+* @Last Modified time: 2015-12-24 22:27:24
 */
 
 'use strict';
 
 function pageSet(source, destination){
-			document.getElementById(destination).innerHTML = document.getElementById(source).innerHTML;
-		}
+	document.getElementById(destination).innerHTML = document.getElementById(source).innerHTML;
+}
 
-		window.onload = function() {
-			pageSet('content_home', 'page');
-		};
+window.onload = function() {
+	pageSet('content_home', 'page');
+};
 
-		function hideMenu(){
-			document.getElementById('toggle').checked = false;
-		}
+function hideMenu(){
+	document.getElementById('toggle').checked = false;
+}
+
+function curYear(){
+	var curDate = new Date();
+	var curYear = curDate.getFullYear();
+	if(curYear == 2015){
+		document.write('2015');
+	} else{
+		document.write('2015 - ' + curYear);
+	}
+}
